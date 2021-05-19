02a7906e148f33f7ad924bd2e5c692c97e4f9389
Jaise Thomas <jaise.thomas@aot-technologies.com>
2021-05-18 23:48:23 -0700

    initaial commit

---
+++ src/main/java/com/camunda/demo/SimpleDemo/CheckWeatherDelegate.java
@@ -0,0 +1,20 @@
+package com.camunda.demo.SimpleDemo;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+import camundajar.impl.scala.util.Random;
+
+public class CheckWeatherDelegate implements JavaDelegate {
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		
+		Random rando = new Random();
+
+		execution.setVariable("name", "Jaise");
+		execution.setVariable("isWeatherOk", rando.nextBoolean());
+		
+	}
+
+}
